7ff11503ad1f6b9c0bae08660c7c6807aff86048
kalidasbm <kalidasmahawarkar@gmail.com>
2020-10-26 12:17:17 +0530

    added filter functionality file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,55 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// filter
+var all = document.getElementById("all");
+var web = document.getElementById("web");
+var graphic = document.getElementById("graphic");
+var flat = document.getElementById("flat");
+var column = document.getElementsByClassName("column");
+all.addEventListener("click", () => {
+  for (i = 0; i < column.length; i++) {
+    if (!column[i].classList.contains('column-show'))
+      column[i].className += " column-show";
+  }
+  all.className = "activeCta";
+  web.className = "";
+  graphic.className = "";
+  flat.className = "";
+})
+web.addEventListener("click", () => {
+  for (i = 0; i < column.length; i++) {
+    if (!column[i].classList.contains('web')) {
+      column[i].classList.remove('column-show');
+    } else {
+      column[i].className += " column-show";
+    }
+  }
+  all.className = "";
+  web.className = "activeCta";
+  graphic.className = "";
+  flat.className = "";
+})
+graphic.addEventListener("click", () => {
+  for (i = 0; i < column.length; i++) {
+    if (!column[i].classList.contains('graphic')) {
+      column[i].classList.remove('column-show');
+    } else {
+      column[i].className += " column-show";
+    }
+  }
+  all.className = "";
+  web.className = "";
+  graphic.className = "activeCta";
+  flat.className = "";
+})
+flat.addEventListener("click", () => {
+  for (i = 0; i < column.length; i++) {
+    if (!column[i].classList.contains('flat')) {
+      column[i].classList.remove('column-show');
+    } else {
+      column[i].className += " column-show";
+    }
+  }
+  all.className = "";
+  web.className = "";
+  graphic.className = "";
+  flat.className = "activeCta";
+})
